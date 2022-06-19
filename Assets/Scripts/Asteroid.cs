a3c076f79d4fd4b49e4459611a2606b4bdb5b9bd
Azure Sky <azuremmii@hotmail.com>
2022-06-19 13:48:31 +0100

    Added asteroids to game, and collison

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,23 @@
+
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
+
+        if(playerHealth == null) {return;}
+        
+        playerHealth.Crash();
+        
+        
+        
+    }
+
+    private void OnBecameInvisible() 
+    {
+        Destroy(gameObject);    
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public void Crash()
+    {
+        gameObject.SetActive(false);
+    }
+}
